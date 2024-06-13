cab67f3e89ae97141ac5eac63a77112d6bcf280d
admin <admin>
2024-06-13 22:09:13 +0800

    no message

---
+++ pages/home-page/gf_level.vue
@@ -0,0 +1,49 @@
+<template>
+	<view class="container">
+		<view class="back_head" @click="too('/','bac')">
+			<u--text prefixIcon="arrow-left" iconStyle="font-size: 38rpx;color:#ebebeb" color="#ebebeb" align="left"
+				text=""></u--text>
+			<u--text color="#fff" bold align="center" text="等级提升"></u--text>
+			<u--text color="#d7d7d7" align="right" text=""></u--text>
+		</view>
+		<u-swiper previousMargin="30"
+                nextMargin="30"
+                circular :list="list1" @change="change" @click="click"></u-swiper>
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				list1: [
+					'https://cdn.uviewui.com/uview/swiper/swiper1.png',
+					'https://cdn.uviewui.com/uview/swiper/swiper2.png',
+					'https://cdn.uviewui.com/uview/swiper/swiper3.png',
+				]
+			};
+		}
+	}
+</script>
+
+<style lang="scss">
+	.back_head {
+		position: fixed;
+		// top: 40px;
+		left: 0;
+		width: 100%;
+		height: 80rpx;
+		box-sizing: border-box;
+		padding: 50rpx 30rpx;
+		display: flex;
+		align-items: center;
+		justify-content: space-between;
+		color: #ffffff;
+		z-index: 999;
+	}
+.container{
+		padding-bottom: 20px;
+		background: linear-gradient( 166deg, #40496F 0%, #1C2D55 100%);
+		min-height: 100vh;
+	}
+</style>
