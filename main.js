04d575b93e88be0d8b37e49c91b6f7d660f7121b
nico <308878328@qq.com>
2024-08-05 16:00:41 +0700

    initial

--- main.js
+++ main.js
@@ -1,6 +1,5 @@
 import App from './App'
 
-// #ifndef VUE3
 import Vue from 'vue'
 import './uni.promisify.adaptor'
 
@@ -11,14 +10,3 @@ const app = new Vue({
   ...App
 })
 app.$mount()
-// #endif
-
-// #ifdef VUE3
-import { createSSRApp } from 'vue'
-export function createApp() {
-  const app = createSSRApp(App)
-  return {
-    app
-  }
-}
-// #endif
