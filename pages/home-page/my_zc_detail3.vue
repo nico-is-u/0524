8f1dda8d4062b1f2b6e2c39f781912cc81d1d784
nico <308878328@qq.com>
2024-08-14 19:03:06 +0700

    首页新增了人民币汇率部分

--- pages/home-page/my_zc_detail3.vue
+++ pages/home-page/my_zc_detail3.vue
@@ -5,11 +5,32 @@
         <!-- 头部 -->
 		<view class="page-head">
             <!-- 顶栏 -->
-			<nNavbar :title="navTitle" :showBackBtn="true" :back="false" :backFunc="backPrev"></nNavbar>
+			<nNavbar title="我的项目" :showBackBtn="true" :back="false" :backFunc="backPrev"></nNavbar>
             <!-- 小菜单 -->
+            <view class="padding-box-3">
+                <view class="menu-card flex flex-column">
+
+                    <view class="row flex flex-between flex-y-center">
+                        <view class="menu-item" @click="too('/pages/home-page/my_zc_detail')">
+                            <view class="label">{{userInfo && userInfo.yun ? (parseFloat(userInfo.yun).toFixed(2)) : '0.00'}}</view>
+                            <view class="value">外汇储备</view>
+                        </view>
+                        <view class="line"></view>
+                        <view class="menu-item" @click="too('/pages/home-page/my_zc_detail2?type=3')">
+                            <view class="label">{{userInfo && userInfo.total_foreign ? (parseFloat(userInfo.total_foreign).toFixed(2)) : '0.00'}}</view>
+                            <view class="value">短期理财</view>
+                        </view>
+                        <view class="line"></view>
+                        <view class="menu-item">
+                            <view class="label"></view>
+                            <view class="value"></view>
+                        </view>
+                    </view>
+                    
+                </view>
+                
+            </view>
         </view>
-        <!-- 加载动画 -->
-        <u-loading-page :loading="loading" style="z-index: 3"></u-loading-page>
     </view>
 </template>
 
